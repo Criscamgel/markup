9b6c0672c10bbf606de61e7c294cd9c9350a784e
Camilo Guio <camiloguio@gmail.com>
2020-05-29 16:38:44 -0500

    Incorporación Modal Edad

--- src/app/components/home-contigencia/home-contigencia.component.ts
+++ src/app/components/home-contigencia/home-contigencia.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalContentComponent } from '../shared/modal-content/modal-content.component';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-home-contigencia',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeContigenciaComponent implements OnInit {
 
-  constructor() { }
+  ageCalc;
+  diferencia;
+  showAge;
+
+
+  constructor(public dialog: MatDialog) {
+
+    let dialogRef = this.dialog.open(ModalContentComponent, {
+      width: '300px',
+      data: {ageCalc: this.ageCalc},
+      disableClose: true
+  });
+
+    dialogRef.afterClosed().subscribe(result => {   
+      this.ageCalc = result.ageCalc;
+      this.ageCalculator();         
+    })
+
+   }
 
   ngOnInit() {
+  }
+
+  ageCalculator() {
+    if (this.ageCalc) {      
+
+      var showAge;
+
+      const convertAge = this.ageCalc.getTime();
+      const timeDiff = Date.now() - convertAge;      
+      showAge = Number(((timeDiff / (1000 * 60 * 60 * 24 * 365.25)).toFixed(2)));
+      this.diferencia = Math.round((75 - showAge) * 12);
+      this.showAge = Math.round(showAge);
+
+    }
+
   }
 
 }
