d4ac25a0eb655283ab94281b1a3054048818a134
Camilo Guio <camiloguio@gmail.com>
2020-05-29 21:24:51 -0500

    Logos Cambiados

--- src/app/components/shared/menu/menu.component.ts
+++ src/app/components/shared/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { constantes } from 'src/app/constantes/constantes';
 
 @Component({
   selector: 'app-menu',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+
+  const = constantes;
 
   constructor() { }
 
